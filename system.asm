61bfb1397805f5891e96af293b7cae622a939362
Matt Song <mattsong@Matts-MacBook-Air.local>
2021-09-24 01:05:12 +0800

    COS-0.4

--- system.asm
+++ system.asm
@@ -12,7 +12,7 @@ TASK0_DATA_SEL equ 0x0017
 ;######## INIT ########
 ;######################
 
-start:
+init:
     mov ax, DATA_SEL
     mov ds, ax
     mov ss, ax
@@ -27,22 +27,20 @@ load_gdt:
     ret
 
 load_idt:
-    mov ax, DATA_SEL
-    mov ds, ax
     mov eax, CODE_SEL                       ;; prepare descriptor
     shl eax, 16
     mov ax, interrupt_ignore                 
     mov ebx, 0x8e00
     mov ecx, 256                            ;; copy to idt  
     mov esi, idt
-f_1:
+f_li:
     mov dword ds:[esi], eax
     mov dword ds:[esi+4], ebx
     add esi, 8
     dec ecx
-    jnz f_1
-    mov eax, CODE_SEL
-    shl eax, 16                      ;; prepare syscall_gate descriptor
+    jnz f_li
+    mov eax, CODE_SEL                       ;; prepare syscall_gate descriptor
+    shl eax, 16
     mov ax, sys_call
     mov edx, 0x0000ef00 
     mov ecx, 0x80
@@ -69,7 +67,6 @@ switch_to_task0:
     ltr ax
     mov ax, LDT0_SEL
     lldt ax
-    sti
     push dword TASK0_DATA_SEL
     push dword init_stack
     pushfd
@@ -89,58 +86,25 @@ task0:
 ;######################
 
 interrupt_ignore:
-    iret
+    jmp $
 
 sys_call:
-    call say_hello
-f_2:
-    jmp f_2
-
-;######################
-;######## TEST ########
-;######################
-
-say_hello:
-    mov ax, SCRN_SEL
-    mov gs, ax
-    call flush
-    mov byte gs:[0], 'C'
-    mov byte gs:[2], 'O'
-    mov byte gs:[4], 'S'
-    mov byte gs:[6], ' '
-    mov byte gs:[8], 'i'
-    mov byte gs:[10], 's'
-    mov byte gs:[12], ' '
-    mov byte gs:[14], 'L'
-    mov byte gs:[16], 'o'
-    mov byte gs:[18], 'a'
-    mov byte gs:[20], 'd'
-    mov byte gs:[22], 'i'
-    mov byte gs:[24], 'n'
-    mov byte gs:[26], 'g'
-    mov byte gs:[28], '.'
-    mov byte gs:[30], '.'
-    mov byte gs:[32], '.'
-    ret
-
-flush:
-    mov ax, SCRN_SEL
-    mov gs, ax
-    mov bl, 32
-    mov cx, 80 * 25
-    mov si, 0
-f_3: 
-    mov gs:[si], bl
-    add si, 2
-    dec cx
-    jnz f_3
-    ret
+    call clear_screen
+    mov eax, 0x41424344
+    call print_char
+    call println
+    call print_char
+    call printtb
+    call print_reg
+    call printtb
+    call print_reg
+    jmp $
 
 ;######################
 ;##### INIT_STACK #####
 ;######################
 
-    times 32 dd 0
+    times 1024 db 0
 init_stack:
 
 ;######################
@@ -165,7 +129,7 @@ gdt:
     dw 0x0002, 0x8000, 0x920b, 0x00c0
     dw 0x0068, tss0, 0xe900, 0x0000
     dw 0x0040, ldt0, 0xe200, 0x0000
-    times 250 dd 0, 0
+    times 256*8-($-gdt) db 0
 
 ;######################
 ;#### PAGE TABLE ######
@@ -175,8 +139,6 @@ gdt:
 ;######################
 ;####### TASK #########
 ;######################
-
-    times 4096*15 db 0
 
 tss0:
     dd 0
@@ -196,6 +158,167 @@ ldt0:
 task0_kernel_stack:
 
     times 4096*63 db 0 
+
+;######################
+;######## PRINT #######
+;######################
+
+tty_pos:
+    dw 0
+
+println:
+    push gs
+    push ds
+    push ebx
+    push eax
+    mov ax, SCRN_SEL
+    mov gs, ax
+    mov ax, DATA_SEL
+    mov ds, ax
+    mov ax, ds:[tty_pos]
+    mov bl, 0x50
+    div bl
+    add al, 1
+    mul bl
+    mov ds:[tty_pos], ax
+    pop eax
+    pop ebx
+    pop ds
+    pop gs
+    ret
+
+printtb:
+    push gs
+    push ds
+    push eax
+    mov ax, SCRN_SEL
+    mov gs, ax
+    mov ax, DATA_SEL
+    mov ds, ax
+    mov ax, ds:[tty_pos]
+    add ax, 4
+    mov ds:[tty_pos], ax
+    pop eax
+    pop ds
+    pop gs
+    ret
+
+print_char:
+    push gs
+    push ds
+    push esi
+    push ebp
+    push eax 
+    mov ax, SCRN_SEL
+    mov gs, ax
+    mov ax, DATA_SEL
+    mov ds, ax
+    mov ebp, esp
+    mov si, ds:[tty_pos]
+    shl si, 1
+    mov al, ds:[ebp+3]
+    mov gs:[si], al
+    mov al, ds:[ebp+2]
+    mov gs:[si+2], al
+    mov al, ds:[ebp+1]
+    mov gs:[si+4], al
+    mov al, ds:[ebp]
+    mov gs:[si+6], al
+    shr si, 1
+    add si, 4
+    mov ds:[tty_pos], si
+    pop eax
+    pop ebp
+    pop esi
+    pop ds
+    pop gs
+    ret
+
+clear_screen:
+    push gs
+    push ax
+    push bx
+    push cx
+    push si
+    mov ax, SCRN_SEL
+    mov gs, ax
+    mov bl, 32
+    mov cx, 80 * 25
+    mov si, 0
+f_cs: 
+    mov gs:[si], bl
+    add si, 2
+    dec cx
+    jnz f_cs
+    pop si
+    pop cx
+    pop bx
+    pop ax
+    pop gs
+    ret
+
+print_reg:
+    push gs
+    push ds
+    push esi
+    push ebp
+    push eax
+    mov ax, DATA_SEL
+    mov ds, ax
+    mov ax, SCRN_SEL
+    mov gs, ax
+    mov ebp, esp
+    mov si, ds:[tty_pos]    
+    shl si, 1    
+    mov byte al, ds:[ebp+3] 
+    call f_pr_1
+    add si, 4
+    mov byte al, ds:[ebp+2]
+    call f_pr_1
+    add si, 4
+    mov byte al, ds:[ebp+1]
+    call f_pr_1
+    add si, 4
+    mov byte al, ds:[ebp]
+    call f_pr_1
+    add si, 4
+    shr si, 1
+    mov ds:[tty_pos], si
+    pop eax
+    pop ebp
+    pop esi
+    pop ds
+    pop gs
+    ret
+f_pr_1:
+    push ax
+    push bx
+    push si
+    mov bl, al
+    and bl, 0x0f
+    add si, 2
+    call f_pr_2
+    mov bl, al
+    shr bl, 4
+    sub si, 2
+    call f_pr_2
+    pop si
+    pop bx
+    pop ax
+    ret
+f_pr_2:
+    push bx
+    cmp bl, 0x09
+    ja f_pr_3
+    add bl, 48
+    mov byte gs:[si], bl
+    pop bx
+    ret
+f_pr_3:
+    add bl, 55
+    mov byte gs:[si], bl
+    pop bx
+    ret
 
 ;######################
 ;######## END #########
