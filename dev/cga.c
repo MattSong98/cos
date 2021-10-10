46646497e76a73df0da8f47a8494773b078d7b7c
mattsong_arch <407735995@qq.com>
2021-10-11 03:24:19 +0800

    snapshot-date:Mon Oct 11 03:24:19 AM CST 2021

--- dev/cga.c
+++ dev/cga.c
@@ -8,19 +8,19 @@
 #define CGA_STD_ATR 0x07
 
 extern void 
-_copy_to_cga (unsigned char c, unsigned char atr, unsigned short pos);
+_copy_to_cga (uchar c, uchar atr, ushort pos);
 
-static unsigned char buf[CGA_SIZE];
-static unsigned char atr[CGA_SIZE];
-static unsigned short pos;
+static uchar buf[CGA_SIZE];
+static uchar atr[CGA_SIZE];
+static ushort pos;
 
 static void update_cursor(ushort);
-static void write_char_to_buf(unsigned char);
+static void write_char_to_buf(uchar);
 static void scroll_up();
 static void flush();
 
 void init_cga() {
-	for (unsigned short i = 0; i < CGA_SIZE; i++) {
+	for (ushort i = 0; i < CGA_SIZE; i++) {
 		buf[i] = ' ';
 		atr[i] = CGA_STD_ATR;
 	}
@@ -32,11 +32,11 @@ void write_cga(void *ptr, int type) {
 
 	if (type == TYPE_HEX) {
 		// assume sizeof(TYPE_HEX) == 4
-		unsigned char digits[8];
-		unsigned long hex = *(unsigned long *)(ptr);
-		unsigned char dig;
-		for (int i = 0; i < 8; i++) {
-			dig = (unsigned char)(hex % 16); 
+		uchar digits[8];
+		uint hex = *(uint *)(ptr);
+		uchar dig;
+		for (uchar i = 0; i < 8; i++) {
+			dig = (uchar)(hex % 16); 
 			if (dig < 10) dig += 48;
 			else dig += 55;
 			digits[i] = dig;
@@ -44,7 +44,7 @@ void write_cga(void *ptr, int type) {
 		}
 		write_char_to_buf('0');
 		write_char_to_buf('x');
-		for (int i = 7; i >= 0; i--) {
+		for (uchar i = 7; i >= 0; i--) {
 			write_char_to_buf(digits[i]);
 		}
 		flush();
@@ -53,7 +53,7 @@ void write_cga(void *ptr, int type) {
 
 void clear_cga() {
 	pos = 0;
-	for (unsigned short i = 0; i < CGA_SIZE; i++) {
+	for (ushort i = 0; i < CGA_SIZE; i++) {
 		buf[i] = ' ';
 	}
 	flush();
@@ -67,12 +67,12 @@ void update_cursor(ushort pos) {
 }
 
 void flush() {
-	for (unsigned short i = 0; i < CGA_SIZE; i++) {
+	for (ushort i = 0; i < CGA_SIZE; i++) {
 		_copy_to_cga(buf[i], atr[i], i);
 	}
 }
 
-void write_char_to_buf(unsigned char c) {
+void write_char_to_buf(uchar c) {
 	if (pos == CGA_SIZE) {
 		scroll_up();
 	}
@@ -80,10 +80,10 @@ void write_char_to_buf(unsigned char c) {
 }
 
 void scroll_up() {
-	for (unsigned short i = 0; i < CGA_SIZE - CGA_COL_SIZE; i++) {
+	for (ushort i = 0; i < CGA_SIZE - CGA_COL_SIZE; i++) {
 		buf[i] = buf[i+CGA_COL_SIZE];
 	}
-	for (unsigned short i = CGA_SIZE - CGA_COL_SIZE; i < CGA_SIZE; i++) {
+	for (ushort i = CGA_SIZE - CGA_COL_SIZE; i < CGA_SIZE; i++) {
 		buf[i] = ' ';
 	}
 	pos = CGA_SIZE - CGA_COL_SIZE;
