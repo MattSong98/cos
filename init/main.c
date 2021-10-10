ee6679f3e43fd5de2867d729c5fe40816ac910dd
mattsong_arch <407735995@qq.com>
2021-10-10 23:55:32 +0800

    snapshot-date:Sun Oct 10 11:55:32 PM CST 2021

--- init/main.c
+++ init/main.c
@@ -14,7 +14,7 @@ void _copy_to_cga(unsigned char, unsigned char, unsigned short);
 int
 main(void) {
 	
-	_copy_to_cga('A', 0xb, 2000); 
+	_copy_to_cga('A', 0xb, 1999); 
 // 	init_tty();
 	while(1);	
 
