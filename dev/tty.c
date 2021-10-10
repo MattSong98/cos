4660753749c62a3366db6ea7580c83af4f2d8758
mattsong_arch <407735995@qq.com>
2021-10-11 02:16:41 +0800

    snapshot-date:Mon Oct 11 02:16:41 AM CST 2021

--- dev/tty.c
+++ dev/tty.c
@@ -1,53 +1,36 @@
-#define TYPE_INT 0
-#define TYPE_USD_INT 1
-#define TYPE_SHT 2
-#define TYPE_USD_SHT 3
-#define TYPE_LNG 4
-#define TYPE_USD_LNG 5
-#define TYPE_HEX 6
-#define TYPE_STR 7
-#define TYPE_CHAR 8
+#define CGA_SIZE 80*25
+#define CGA_ROW_SIZE 25
+#define CGA_COL_SIZE 80
+#define CGA_STD_ATR 0x07
 
-#define TTY_SIZE 80*25
-#define TTY_ROW_SIZE 25
-#define TTY_COL_SIZE 80
-#define TTY_STD_ATR 0x07
+#define TYPE_HEX 0
+#define TYPE_STR 1
 
 extern void 
 _copy_to_cga (unsigned char c, unsigned char atr, unsigned short pos);
 
-static unsigned char buf[TTY_SIZE];
-static unsigned char atr[TTY_SIZE];
+static unsigned char buf[CGA_SIZE];
+static unsigned char atr[CGA_SIZE];
 static unsigned short pos;
 
-static void print_to_tty(unsigned char);
+static void write_char_to_buf(unsigned char);
 static void scroll_up();
 static void flush();
 
-extern void init_tty();
-extern void print_hello();
-extern void write_tty(void *ptr, int type);
+extern void init_cga();
+extern void write_cga(void *ptr, int type);
 extern void clear();
 
-void init_tty() {
-	for (unsigned short i = 0; i < TTY_SIZE; i++) {
+void init_cga() {
+	for (unsigned short i = 0; i < CGA_SIZE; i++) {
 		buf[i] = ' ';
-		atr[i] = TTY_STD_ATR;
+		atr[i] = CGA_STD_ATR;
 	}
 	flush();
 }
 
-void print_hello() {
-	print_to_tty('H');
-	print_to_tty('e');
-	print_to_tty('l');
-	print_to_tty('l');
-	print_to_tty('o');
-	print_to_tty('\n');
-	flush();
-}
+void write_cga(void *ptr, int type) {
 
-void write_tty(void *ptr, int type) {
 	if (type == TYPE_HEX) {
 		// assume sizeof(TYPE_HEX) == 4
 		unsigned char digits[8];
@@ -60,10 +43,10 @@ void write_tty(void *ptr, int type) {
 			digits[i] = dig;
 			hex = hex / 16;
 		}
-		print_to_tty('0');
-		print_to_tty('x');
+		write_char_to_buf('0');
+		write_char_to_buf('x');
 		for (int i = 7; i >= 0; i--) {
-			print_to_tty(digits[i]);
+			write_char_to_buf(digits[i]);
 		}
 		flush();
 	} 
@@ -71,32 +54,32 @@ void write_tty(void *ptr, int type) {
 
 void clear() {
 	pos = 0;
-	for (unsigned short i = 0; i < TTY_SIZE; i++) {
+	for (unsigned short i = 0; i < CGA_SIZE; i++) {
 		buf[i] = ' ';
 	}
 	flush();
 }
 
 void flush() {
-	for (unsigned short i = 0; i < TTY_SIZE; i++) {
+	for (unsigned short i = 0; i < CGA_SIZE; i++) {
 		_copy_to_cga(buf[i], atr[i], i);
 	}
 }
 
-void print_to_tty(unsigned char c) {
-	if (pos == TTY_SIZE) {
+void write_char_to_buf(unsigned char c) {
+	if (pos == CGA_SIZE) {
 		scroll_up();
 	}
 	buf[pos++] = c;
 }
 
 void scroll_up() {
-	for (unsigned short i = 0; i < TTY_SIZE - TTY_COL_SIZE; i++) {
-		buf[i] = buf[i+TTY_COL_SIZE];
+	for (unsigned short i = 0; i < CGA_SIZE - CGA_COL_SIZE; i++) {
+		buf[i] = buf[i+CGA_COL_SIZE];
 	}
-	for (unsigned short i = TTY_SIZE - TTY_COL_SIZE; i < TTY_SIZE; i++) {
+	for (unsigned short i = CGA_SIZE - CGA_COL_SIZE; i < CGA_SIZE; i++) {
 		buf[i] = ' ';
 	}
-	pos = TTY_SIZE - TTY_COL_SIZE;
+	pos = CGA_SIZE - CGA_COL_SIZE;
 }
 
--- init/main.c
+++ init/main.c
@@ -3,43 +3,22 @@
 
 #include "mm.h"
 
-void init_tty(void);
-void print_hello(void);
-void write_tty(void *, int);
-void _println(void);
-void _printtb(void);
-void _print_reg(unsigned);
-void _copy_to_cga(unsigned char, unsigned char, unsigned short);
+void init_cga(void);
+void write_cga(void *, int);
 
 // entry point here
 
 int
 main(void) {
 	
-    init_tty();
+    init_cga();
 	unsigned long a = 0x12345678;
-	print_hello();
-	write_tty(&a, 6);
+	write_cga(&a, 0);
 	while(1);	
 
-	_print_reg(sizeof(int));
-	_println();
 
 	init_memory();
 
-	unsigned long ad1 = get_free_page();
-	_print_reg(ad1);
-	_println();
-
-	unsigned long ad2 = get_free_page();
-	_print_reg(ad2);
-	_println();
-
-	free_page(ad1);
-
-	unsigned long ad3 = get_free_page();
-	_print_reg(ad3);
-	_println();
 	
 	while (1);
 	return 0;
