f42cf48d380ccb0c66415dead0e059b573bb42a8
mattsong_arch <407735995@qq.com>
2021-10-11 07:09:12 +0800

    snapshot-date:Mon Oct 11 07:09:12 AM CST 2021

--- dev/keyboard.c
+++ dev/keyboard.c
@@ -10,17 +10,18 @@ void init_ps2_ctlr() {
 	// Determine if the PS/2 Controller exists : Skip Over
 
 	// Disable devices: 
-		outb(0x64, 0xAD);
-		outb(0x64, 0xA7);
+		// outb(0x64, 0xAD);
+		// outb(0x64, 0xA7);
 
 	// Flush the Output Buffer
 		do {
 			uchar status = inb(0x64);
+			uint hex = (uint) status;
+			write_cga(&hex, TYPE_HEX);
 			status = status & 0x01;
 			if (status == 1) {
 				uchar data = inb(0x60);
 				uint hex = (uint) data;
-				write_cga(&data, TYPE_CHAR);
 				write_cga(&hex, TYPE_HEX);
 			}
 		} while (1);
