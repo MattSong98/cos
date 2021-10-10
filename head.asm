f5e4d8973e47f544a004a0d3333ddfb5e73f8b7d
mattsong_arch <407735995@qq.com>
2021-10-10 17:00:56 +0800

    snapshot-date:Sun Oct 10 05:00:56 PM CST 2021

--- head.asm
+++ head.asm
@@ -8,9 +8,15 @@ SCRN_SEL equ 0x0018
 ;################################### INIT #####################################
 ;##############################################################################
 
+segment .data
+
+a dd 0x01
+b dd 0x02
+
+
 segment .text
-	global init, _println, _printtb, _print_reg
-	extern main
+	global init
+	extern add
 
 page_dir:
 init:
@@ -19,37 +25,18 @@ init:
     mov ss, ax
     mov esp, init_stack 
     call load_gdt
-    call load_idt
     call flush_sreg
     call clear_screen
-	jmp setup_page_dir
+test:
+	push dword [a]
+	push dword [b]
+	call add
+	call print_reg
+	jmp $
+	
 
 load_gdt:
     lgdt ds:[gdt_48]
-    ret
-
-load_idt:
-    mov eax, CODE_SEL                       ;; prepare descriptor
-    shl eax, 16
-    mov ax, interrupt_ignore                 
-    mov ebx, 0x8e00
-    mov ecx, 256                            ;; copy to idt  
-    mov esi, idt
-f_li:
-    mov dword ds:[esi], eax
-    mov dword ds:[esi+4], ebx
-    add esi, 8
-    dec ecx
-    jnz f_li
-    mov eax, CODE_SEL                       ;; prepare syscall_gate descriptor
-    shl eax, 16
-    mov ax, sys_call
-    mov edx, 0x0000ef00 
-    mov ecx, 0x80
-    lea esi, [idt+ecx*8]
-    mov dword ds:[esi], eax                 ;; move to 0x80
-    mov dword ds:[esi+4], edx  
-    lidt ds:[idt_48]         
     ret
 
 flush_sreg:
@@ -62,99 +49,10 @@ flush_sreg:
     ret
 
 ;##############################################################################
-;############################## PAGE TABLE ####################################
-;##############################################################################
-
-align 0x1000
-
-page_table:
-	times 0x10000 db 0
-
-setup_page_dir:
-	mov ax, DATA_SEL
-	mov ds, ax
-
-	mov esi, page_dir
-f_spd_1:
-	mov dword ds:[esi], 0
-	add esi, 4	
-	cmp esi, 0x1000
-	jl f_spd_1
-
-	mov ecx, 1
-	mov esi, page_dir
-f_spd_2:
-	mov eax, ecx
-	shl eax, 12
-	inc eax
-	mov ds:[esi], eax
-	add esi, 4
-	inc ecx
-	cmp ecx, 0x10
-	jle f_spd_2
-
-setup_page_table:
-	mov ecx, 0
-	mov esi, page_table
-f_spt:
-	mov eax, ecx
-	shl eax, 12
-	inc eax
-	mov ds:[esi], eax
-	add esi, 4
-	inc ecx
-	cmp ecx, 0x4000
-	jl f_spt
-
-	xor eax, eax
-	mov cr3, eax
-	mov eax, cr0
-	or eax, 0x80000000
-	mov cr0, eax
-
-	mov eax, ds:[60]
-	call print_reg
-	call println
-	mov eax, ds:[0x109e0]
-	call print_reg
-	call println
-	mov eax, ds:[0x1000]
-	call print_reg
-	call println
-	mov eax, ds:[0x2000]
-	call print_reg
-	call println
-	mov eax, ds:[0x3000]
-	call print_reg
-	call println
-	mov eax, ds:[0xa000]
-	call print_reg
-	call println
-	mov eax, ds:[0xc000]
-	call print_reg
-	call println
-	mov eax, ds:[0x10000]
-	call print_reg
-	call println
-	jmp $
-	call main
-
-;##############################################################################
-;################################ INTERRUPT ###################################
-;##############################################################################
-
-interrupt_ignore:
-    iret
-
-sys_call:
-    iret
-
-time_interrupt:
-    iret
-
-;##############################################################################
 ;############################### INIT STACK ###################################
 ;##############################################################################
+
+align 4
 
     times 1024 db 0
 init_stack:
@@ -163,16 +61,11 @@ init_stack:
 ;############################### IDT & GDT ####################################
 ;##############################################################################
 
-idt_48:
-    dw 256*8-1
-    dd idt
-
 gdt_48:
     dw 256*8-1
     dd gdt
 
-idt:
-    times 256 dd 0, 0
+align 8
 
 gdt:
     dw 0, 0, 0, 0
@@ -185,93 +78,10 @@ gdt:
 ;################################# PRINT ######################################
 ;##############################################################################
 
+align 4
+
 tty_pos:
     dw 0
-
-_println:
-	call println
-	ret
-
-_printtb:
-	call printtb
-	ret
-
-_print_reg:
-	push ebp
-	mov ebp, esp
-	mov eax, [ebp+8]
-	call print_reg
-	mov esp, ebp
-	pop ebp
-	ret
-
-println:
-    push gs
-    push ds
-    push ebx
-    push eax
-    mov ax, SCRN_SEL
-    mov gs, ax
-    mov ax, DATA_SEL
-    mov ds, ax
-    mov ax, ds:[tty_pos]
-    mov bl, 0x50
-    div bl
-    add al, 1
-    mul bl
-    mov ds:[tty_pos], ax
-    pop eax
-    pop ebx
-    pop ds
-    pop gs
-    ret
-
-printtb:
-    push gs
-    push ds
-    push eax
-    mov ax, SCRN_SEL
-    mov gs, ax
-    mov ax, DATA_SEL
-    mov ds, ax
-    mov ax, ds:[tty_pos]
-    add ax, 4
-    mov ds:[tty_pos], ax
-    pop eax
-    pop ds
-    pop gs
-    ret
-
-print_char:
-    push gs
-    push ds
-    push esi
-    push ebp
-    push eax 
-    mov ax, SCRN_SEL
-    mov gs, ax
-    mov ax, DATA_SEL
-    mov ds, ax
-    mov ebp, esp
-    mov si, ds:[tty_pos]
-    shl si, 1
-    mov al, ds:[ebp+3]
-    mov gs:[si], al
-    mov al, ds:[ebp+2]
-    mov gs:[si+2], al
-    mov al, ds:[ebp+1]
-    mov gs:[si+4], al
-    mov al, ds:[ebp]
-    mov gs:[si+6], al
-    shr si, 1
-    add si, 4
-    mov ds:[tty_pos], si
-    pop eax
-    pop ebp
-    pop esi
-    pop ds
-    pop gs
-    ret
 
 clear_screen:
     push gs
--- main.c
+++ main.c
@@ -1,28 +1,9 @@
 // main called by head.s
-// from now on, we stay with C.
+// from now on, we stay with C
 
-int b = 7;
+int array[5];
+int num = 5;
 
-void _println(void);
-void _printtb(void);
-void _print_reg(unsigned);
-
-unsigned int fact(unsigned);
-
-// entry point here
-int
-main(void) {
-	unsigned a = 10;
-	_print_reg(a);
-	_println();
-	_print_reg(fact(a));
-	_println();
-	_print_reg(b);
-	while (1);
-	return 0;
-}
-
-unsigned fact(unsigned n) {
-	if (n == 1) return 1;
-	else return fact(n-1) * n;
+int add(int a, int b) {
+	return a + b;
 }
