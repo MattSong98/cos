94eee2fff3591678463498c03b61adf7671e4389
mattsong_arch <407735995@qq.com>
2021-10-10 20:37:15 +0800

    snapshot-date:Sun Oct 10 08:37:15 PM CST 2021

--- init/main.c
+++ init/main.c
@@ -3,26 +3,25 @@
 
 #include "mm.h"
 
-int num_data = 7;
-
 void _println(void);
 void _printtb(void);
 void _print_reg(unsigned);
-
-unsigned int fact(unsigned);
 
 // entry point here
 
 int
 main(void) {
-	_print_reg(PAGE_SIZE);
+
+	init_memory();
+
+	unsigned long ad1 = get_free_page();
+	_print_reg(ad1);
 	_println();
-	_print_reg(num_data);
+
+	unsigned long ad2 = get_free_page();
+	_print_reg(ad2);
+	_println();
+
 	while (1);
 	return 0;
-}
-
-unsigned fact(unsigned n) {
-	if (n == 1) return 1;
-	else return fact(n-1) * n;
 }
