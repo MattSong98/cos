82e6daf2a7043a840269db9ffa00af68f3cd5cfc
mattsong_arch <407735995@qq.com>
2021-10-11 02:25:13 +0800

    snapshot-date:Mon Oct 11 02:25:13 AM CST 2021

--- init/main.c
+++ init/main.c
@@ -4,13 +4,14 @@
 #include "mm.h"
 #include "cga.h"
 
-// entry point here
-
 int
 main(void) {
 	
     init_cga();
 	unsigned long a = 0x12345678;
+	write_cga(&a, 0);
+	clear_cga();
+	write_cga(&a, 0);
 	write_cga(&a, 0);
 	while(1);	
 
