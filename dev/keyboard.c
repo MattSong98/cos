fe3f7e2f99ad8d2ed135afbae46f9a4be98444f8
mattsong_arch <407735995@qq.com>
2021-10-11 08:14:20 +0800

    snapshot-date:Mon Oct 11 08:14:20 AM CST 2021

--- dev/keyboard.c
+++ dev/keyboard.c
@@ -6,32 +6,36 @@
 void init_ps2_ctlr() {
 	
 	// initialize USB Controller: Skip Over
-		
 	// Determine if the PS/2 Controller exists : Skip Over
-
-	// Disable devices: 
-		outb(0x64, 0xAD);
-		//outb(0x64, 0xA7);
+	// Disable both devices:
+	while ((inb(0x64) & 0x02) != 0);
+	outb(0x64, 0xAD);
+	while ((inb(0x64) & 0x02) != 0);
+	outb(0x64, 0xA7);
 
 	// Flush the Output Buffer
-		do {
-			uchar status = inb(0x64);
-			// uint hex = (uint) status;
-			// write_cga(&hex, TYPE_HEX);
-			status = status & 0x01;
-			if (status == 1) {
-				uchar data = inb(0x60);
-				uint hex = (uint) data;
-				write_cga(&hex, TYPE_HEX);
-			}
-		} while (1);
+	inb(0x60);
 
 	// Set the Controller Configuration Byte
+	while ((inb(0x64) & 0x02) != 0);
+	outb(0x64, 0x20);
+	while ((inb(0x64) & 0x01) != 1);
+	uchar ccb = inb(0x60);
+	ccb = ccb | 0x51;
+	while ((inb(0x64) & 0x02) != 0);
+	outb(0x64, 0x60);
+	while ((inb(0x64) & 0x02) != 0);
+	outb(0x60, ccb);
 
 	// Perform Controller Self Test
+	while ((inb(0x64) & 0x02) != 0);
+	outb(0x64, 0xAA);
+	while ((inb(0x64) & 0x01) != 1);
+	uchar test_res = inb(0x60);
+	uint hex = (uint) test_res;
+	write_cga(&hex, TYPE_HEX);
 
-	// Dertermine if there are two channels
-
+	// Dertermine if there are two channels; Skip Over
 	// Perform Interface Tests
 
 	// Enable Devices
