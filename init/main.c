1417c034fce94e1d094812accc87dda09c0356a8
mattsong_arch <407735995@qq.com>
2021-10-14 19:20:12 +0800

    snapshot-date:Thu Oct 14 07:20:12 PM CST 2021

--- init/main.c
+++ init/main.c
@@ -8,18 +8,26 @@
 
 int 
 main(void) {
-	char mesg[] = "hello, there!";
-	write_cga(mesg, TYPE_STR);
-	mm_init();
-	write_cga(mesg, TYPE_STR);
-	pic_init();
-	write_cga(mesg, TYPE_STR);
 	init_cga();
-	write_cga(mesg, TYPE_STR);
+	char mesg1[] = "cga ready  ";
+	write_cga(mesg1, TYPE_STR);
+	
+	mm_init();
+	char mesg2[] = "mm ready  ";
+	write_cga(mesg2, TYPE_STR);
+
+	pic_init();
+	char mesg3[] = "pic ready  ";
+	write_cga(mesg3, TYPE_STR);
+
 	kbd_init();
-	write_cga(mesg, TYPE_STR);
+	char mesg4[] = "kbd ready  ";
+	write_cga(mesg4, TYPE_STR);
+
 	idt_init();
-	write_cga(mesg, TYPE_STR);
+	char mesg5[] = "idt ready  ";
+	write_cga(mesg5, TYPE_STR);
+
 	sti();
 	
 	while(1);
